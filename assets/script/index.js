c4e2518c5df1dfe6416b0c8837dc1b9cefc80862
U-Dev <udaythakur.shely@gmail.com>
2023-02-27 22:13:02 -0600

    face-Variation

---
+++ assets/script/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function moody(seed) {
+  const low = document.getElementById("sad");
+  const mid = document.getElementById("meh");
+  const high = document.getElementById("smile");
+  const num = ~~(Math.random() * 2) ? true : false;
+  if (seed === 1) {
+    low.style.display = "none";
+    if (num) {
+      mid.style.display = "block";
+    } else {
+      high.style.display = "block";
+    }
+  }
+  if (seed === 2) {
+    mid.style.display = "none";
+    if (num) {
+      high.style.display = "block";
+    } else {
+      low.style.display = "block";
+    }
+  }
+  if (seed === 3) {
+    high.style.display = "none";
+    if (num) {
+      low.style.display = "block";
+    } else {
+      mid.style.display = "block";
+    }
+  }
+}
